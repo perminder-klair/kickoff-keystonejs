b2af8720aff42bfeac74462ea224570117165a7e
Parminder Klair <perminder.klair@gmail.com>
2017-12-28 12:33:20 +0530

    minor fixes and upgrades

--- graphql/cars/schema.js
+++ graphql/cars/schema.js
@@ -1,21 +1,21 @@
 const typeDefs = `
-type Description {
-    md: String
-    html: String
+type CarDescription {
+  md: String
+  html: String
 }
 
 type Car {
-   id: ID!                # "!" denotes a required field
-   name: String
-   slug: String
-   description: Description
-   price: Int
+  id: ID!                # "!" denotes a required field
+  name: String
+  slug: String
+  description: CarDescription
+  price: Int
 }
 
 # This type specifies the entry points into our API. In this case
 # there is only one - "Cars" - which returns a list of cars.
 type Query {
-   cars(slug: String): [Car]    # "[]" means this is a list of cars
+  cars(slug: String): [Car]    # "[]" means this is a list of cars
 }
 `;
 
